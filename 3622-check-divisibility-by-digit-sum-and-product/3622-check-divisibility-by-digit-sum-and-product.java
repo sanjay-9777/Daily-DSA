ee34dc08f03ec672223d16c64d0ff70fecafe315
Sanjay Mishra <mishrasanjay9777@gmail.com>
2026-08-22 11:43:23 +0530

    Time: 0 ms (100%), Space: 42.4 MB (62.34%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
@@ -1,18 +1,38 @@
+// class Solution {
+//     public boolean checkDivisibility(int n) {
+//         String str = String.valueOf(n);
+
+//         int sum = 0;
+//         int product = 1;        //bcz something * 0 = 0
+
+//         for(int i = 0; i < str.length(); i++){
+//             int digit = str.charAt(i) -'0';     //charAt() gives us a character, not an integer . so '1' -> 1
+//             sum = sum + digit;
+//             product = product * digit;
+
+//         }
+//         return n % (sum + product)==0;
+
+
+//     }
+// }
+
 class Solution {
     public boolean checkDivisibility(int n) {
-        String str = String.valueOf(n);
 
+        int original = n;
         int sum = 0;
-        int product = 1;        //bcz something * 0 = 0
+        int product = 1;
 
-        for(int i = 0; i < str.length(); i++){
-            int digit = str.charAt(i) -'0';     //charAt() gives us a character, not an integer . so '1' -> 1
-            sum = sum + digit;
-            product = product * digit;
+        while (n > 0) {
+            int digit = n % 10;
 
+            sum += digit;
+            product *= digit;
+
+            n = n / 10;
         }
-        return n % (sum + product)==0;
 
-
+        return original % (sum + product) == 0;
     }
 }
